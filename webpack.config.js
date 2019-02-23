a02afb83f7bccfb9174f0266d49a55c135a86491
Aleksandr Morgunov <aleksandrmorgunov@MacBook-Pro-4.local>
2019-02-23 12:22:28 +0200

    add clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
 // Core
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { resolve } = require('path');
+
+module.exports = () => {
+    return {
+        mode:    'none',
+        devtool: false,
+        output: {
+            filename: 'bundle.js',
+            path:     resolve(__dirname, './build'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './static/template.html',
+                title: 'УуууууУУУУ',
+            }),
+            new CleanWebpackPlugin('./build'),
+        ],
+    }
+};
